471fd23454cd35a34ae8962c92cbb1989b49aead
Shrimp-Chip <76146446+Shrimp-Chip@users.noreply.github.com>
2022-01-21 08:17:56 -0500

    Began Work on the Main Menu

--- Assets/Programming/Frontend Systems/ScreenFader.cs
+++ Assets/Programming/Frontend Systems/ScreenFader.cs
@@ -9,7 +9,7 @@ public class ScreenFader : MonoBehaviour
     public static ScreenFader Instance;
     public Image box;
     public RectTransform boxTransform;
-    private float alpha;
+
     public Color faderColor;
     public float fadeTime = 0.5f;
 
@@ -32,8 +32,10 @@ public class ScreenFader : MonoBehaviour
         box.color = new Color(faderColor.r, faderColor.g, faderColor.b, 0);
     }
 
-    public Tween FadeScene(float to)
+    public Tween FadeScene(float to, float from = -1)
     {
+        if (from != -1) box.color = new Color(faderColor.r, faderColor.g, faderColor.b, from);
+        
         box.DOKill();
         return box.DOFade(to, fadeTime).SetEase(Ease.InOutCubic);
     }
---
+++ Assets/Programming/Misc/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Start()
+    {
+        ScreenFader.Instance.FadeScene(0.5f, 1f);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        Level newLevel = LevelManager.Instance.FindLevel(sceneName);
+        LevelManager.Instance.TransitionLevel(newLevel, "Default");
+    }
+}
