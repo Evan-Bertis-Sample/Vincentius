f7fd1176a6d11063f9b928dab5daa2b8b883c197
Shrimp-Chip <76146446+Shrimp-Chip@users.noreply.github.com>
2022-01-28 08:10:17 -0500

    Zoom System

---
+++ Assets/Programming/Backend Systems/CameraZoom/CameraZoom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using DG.Tweening;
+
+public class CameraZoom : MonoBehaviour
+{
+    public static CameraZoom Main;
+
+    public float originalZoom;
+    Tween zoomTween;
+    Camera cam;
+    CinemachineBrain brain;
+    CinemachineVirtualCamera vcam;
+
+    private void Awake()
+    {
+        if (Main == null)
+        {
+            Main = this;
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start() {
+        cam = Camera.main;
+        brain = cam.GetComponent<CinemachineBrain>();
+        originalZoom = (brain.ActiveVirtualCamera as CinemachineVirtualCamera).m_Lens.OrthographicSize;
+        vcam = brain.ActiveVirtualCamera as CinemachineVirtualCamera;
+    }
+
+    public void ZoomCamera(float zoomAmount, float zoomTime)
+    {
+        zoomTween?.Kill();
+        zoomTween = DOTween.To(() => vcam.m_Lens.OrthographicSize, x => vcam.m_Lens.OrthographicSize = x, zoomAmount, zoomTime).SetEase(Ease.InOutBack);
+    }
+
+    public void ResetZoom(float zoomTime = 1f)
+    {
+        zoomTween?.Kill();
+        zoomTween = DOTween.To(() => vcam.m_Lens.OrthographicSize, x => vcam.m_Lens.OrthographicSize = x, originalZoom, zoomTime).SetEase(Ease.InOutBack);
+    }
+}
---
+++ Assets/Programming/Backend Systems/Interactables/ZoomOnTouch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOnTouch : Interactable
+{
+    public float zoomAmount = 4f;
+    public float zoomSpeed = 1f;
+
+    public override void OnContact()
+    {
+        CameraZoom.Main.ZoomCamera(zoomAmount, zoomSpeed);
+    }
+
+    public override void OnExit()
+    {
+        CameraZoom.Main.ResetZoom(zoomSpeed);
+    }
+
+    public override void OnHold()
+    {
+
+    }
+}
